ac666dd675ed9dab6153d6abe31497f16c2f13f5
Menelik_Berhan <MenelikBerhan@gmail.com>
2024-01-19 00:13:31 +0300

    Use IF function instead of if statment

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -6,13 +6,6 @@
  And returns `a / b` or 0 if `b == 0`.
  */
 -- create a function with above specification
-DELIMITER $
 CREATE FUNCTION SafeDiv(num1 INT, num2 INT)
 RETURNS FLOAT DETERMINISTIC
-BEGIN
-    IF num2 = 0 THEN
-        RETURN 0;
-    END IF;
-    RETURN num1 / num2;
-END$
-DELIMITER ;
+    RETURN IF(num2 = 0, 0, num1 / num2);
