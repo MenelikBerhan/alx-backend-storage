5e79da7780e7b3edba94c5e92b596f5c95f9c548
Menelik_Berhan <MenelikBerhan@gmail.com>
2024-01-23 22:58:39 +0300

    Add import guard

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -3,20 +3,20 @@
 Nginx logs stored in `nginx` collection in `logs` MongoDB."""
 from pymongo import MongoClient
 
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    collection = client.logs.nginx
 
-client = MongoClient('mongodb://127.0.0.1:27017')
-collection = client.logs.nginx
+    # number of all logs
+    print('{} logs'.format(collection.count_documents({})))
 
-# number of all logs
-print('{} logs'.format(collection.count_documents({})))
+    # number of logs for each method
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for method in methods:
+        print('\tmethod {}: {}'.format(
+            method, collection.count_documents({'method': method})))
 
-# number of logs for each method
-methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-for method in methods:
-    print('\tmethod {}: {}'.format(
-        method, collection.count_documents({'method': method})))
-
-# number of logs for `GET` method with `/status` path
-print('{} status check'.format(
-    collection.count_documents({'method': 'GET', 'path': '/status'})
-))
+    # number of logs for `GET` method with `/status` path
+    print('{} status check'.format(
+        collection.count_documents({'method': 'GET', 'path': '/status'})
+    ))
