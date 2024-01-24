0f581b934824a35de085f633d3829435e8221224
Menelik_Berhan <MenelikBerhan@gmail.com>
2024-01-25 00:50:50 +0300

    Task 1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,7 +3,7 @@
 Contais a `Cache` class implemented using Redis.
 """
 import redis
-from typing import Union
+from typing import Any, Callable, Optional, Union
 from uuid import uuid4
 
 
@@ -20,3 +20,20 @@ class Cache():
         key = str(uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Callable[[Any], Any]]) -> Any:
+        """Takes a `key` string and an optional callable `fn`. Gets value
+        associated with `key` from Redis and type cast the vaule using `fn`
+        before returning it."""
+        value = self._redis.get(key)
+        if value is None or fn is None:
+            return value
+        return fn(value)
+
+    def get_str(self, value):
+        """Casts value to a string"""
+        pass
+
+    def get_int(self, value):
+        """Casts value to an integer"""
+        pass
