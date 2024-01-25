4351287176a804ebf3ce6d2d03f0fc9aba0782a6
Menelik_Berhan <MenelikBerhan@gmail.com>
2024-01-25 04:03:14 +0300

    Use single function w/o a decorator

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,11 +1,9 @@
 import redis
 import requests
-from typing import Dict
 
 
 def get_page(url: str) -> str:
-    """Obtain the HTML content of `url` and return it, with caching and access
-    count tracking."""
+    """Obtain the HTML content of `url` and return it, with caching and access count tracking."""
     cache = redis.Redis()
 
     # Check if the URL is already cached
@@ -13,19 +11,11 @@ def get_page(url: str) -> str:
     if cached_response:
         return cached_response.decode()
 
-    # Increment the access count for the URL
-    access_count_key = f'count:{url}'
-    access_count = cache.incr(access_count_key, 1)
-
-    if access_count == 1:
-        # Make the request and cache the response if it's the first access
-        response = requests.get(url)
-        if response.status_code == 200:
-            response_text = response.text
-            cache.set(url, response_text, ex=10)
-            return response_text
-        else:
-            return f"Error: {response.status_code}"
+    # Make the request and cache the response
+    response = requests.get(url)
+    if response.status_code == 200:
+        response_text = response.text
+        cache.set(url, response_text, ex=10)
+        return response_text
     else:
-        # Return an error message if the URL was accessed before but not cached
-        return "Error: URL already accessed before but not cached."
+        return f"Error: {response.status_code}"
